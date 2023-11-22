2e0a2c745fc6242995e30758184ca6e9faba246a
Ranjan Gupta <113838870+Ranjangupta550@users.noreply.github.com>
2023-11-22 23:54:32 +0530

    Update 5_Calculator.cpp

--- DSA/5_Calculator.cpp
+++ DSA/5_Calculator.cpp
@@ -1,1 +1,40 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char operation;
+  double num1, num2;
+
+  cout << "Enter an operation (+, -, *, /): ";
+  cin >> operation;
+
+  cout << "Enter the first number: ";
+  cin >> num1;
+
+  cout << "Enter the second number: ";
+  cin >> num2;
+
+  double result;
+
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+    default:
+      cout << "Invalid operation" << endl;
+  }
+
+  cout << "The result is: " << result << endl;
+
+  return 0;
+}
