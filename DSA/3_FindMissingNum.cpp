7ee02c1a465f9b7a8c85013119c0ea16d70553e4
Ranjan Gupta <113838870+Ranjangupta550@users.noreply.github.com>
2023-11-22 23:53:26 +0530

    Update 3_FindMissingNum.cpp

--- DSA/3_FindMissingNum.cpp
+++ DSA/3_FindMissingNum.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findMissingNumber(const vector<int>& arr, int N) {
+   
+    int totalSum = (N * (N + 1)) / 2;
+
+  
+    int arraySum = 0;
+    for (int num : arr) {
+        arraySum += num;
+    }
+
+   
+    int missingNumber = totalSum - arraySum;
+
+    return missingNumber;
+}
+
+int main() {
+    // Example
+    vector<int> arr = {1, 2, 4, 6, 3, 7, 8};
+    int N = arr.size() + 1;
+    cout << "Missing number: " << findMissingNumber(arr, N) << endl;
+
+    return 0;
+}
