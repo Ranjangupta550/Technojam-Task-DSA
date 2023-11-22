9c240854a2658df1c01117808b5411509973a2af
Ranjan Gupta <113838870+Ranjangupta550@users.noreply.github.com>
2023-11-22 23:54:03 +0530

    Update 4_TowerTOH.cpp

--- DSA/4_TowerTOH.cpp
+++ DSA/4_TowerTOH.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void towerOfHanoi(int n, int source, int dest, int aux) {
+  if (n == 1) {
+    cout << "Move disk 1 from source " << source << " to dest " << dest << endl;
+    return;
+  }
+
+  towerOfHanoi(n - 1, source, aux, dest);
+  cout << "Move disk " << n << " from source " << source << " to dest " << dest << endl;
+  towerOfHanoi(n - 1, aux, dest, source);
+}
+
+int main() {
+  int n;
+  cout << "Enter the number of disks: ";
+  cin >> n;
+
+  towerOfHanoi(n, 1, 3, 2);
+
+  return 0;
+}
 
