795e8237549597b7278a2df09732e1a647c05901
Ranjan Gupta <113838870+Ranjangupta550@users.noreply.github.com>
2023-11-22 23:58:03 +0530

    Update 9_UniCombionation.cpp

--- DSA/9_UniCombionation.cpp
+++ DSA/9_UniCombionation.cpp
@@ -1,1 +1,57 @@
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void findComb(vector<int>& arr, int t, vector<int>& current, int index) {
+    
+    if (t == 0) {
+        cout << "[";
+        for (int i = 0; i < current.size(); ++i) {
+            cout << current[i];
+            if (i < current.size() - 1) {
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+        return;
+    }
+
+   
+    for (int i = index; i < arr.size(); ++i) {
+      
+        if (arr[i] > t) {
+            continue;
+        }
+
+        
+        current.push_back(arr[i]);
+
+
+        findComb(arr, t - arr[i], current, i);
+
+        
+        current.pop_back();
+    }
+}
+
+void printCombinations(vector<int>& arr, int target) {
+    vector<int> current;
+    findComb(arr, target, current, 0);
+
+    if (current.empty()) {
+        cout << "Empty" << endl;
+    }
+}
+
+int main() {
+    
+    vector<int> arr = {2, 4, 6, 8};
+    int target = 8;
+    
+    cout << "Output: " << endl;
+    printCombinations(arr, target);
+
+    return 0;
+}
