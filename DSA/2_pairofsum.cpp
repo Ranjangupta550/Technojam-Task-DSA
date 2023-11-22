525d5517203a076b08f7255a2dfe31170d181395
Ranjan Gupta <113838870+Ranjangupta550@users.noreply.github.com>
2023-11-22 23:52:59 +0530

    Update 2_pairofsum.cpp

--- DSA/2_pairofsum.cpp
+++ DSA/2_pairofsum.cpp
@@ -1,1 +1,43 @@
 
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int countPairs(int arr[], int size, int K) {
+    int countPairs = 0;
+    int totalSum = 0;
+
+   
+    for (int i = 0; i < size; ++i) {
+        totalSum += arr[i];
+    }
+
+  
+    unordered_set<int> elements;
+
+    for (int i = 0; i < size; ++i) {
+        int complement = totalSum - arr[i] - K;
+
+     
+        if (elements.find(complement) != elements.end()) {
+            countPairs += 1;
+        }
+
+        
+        elements.insert(arr[i]);
+    }
+
+    return countPairs;
+}
+
+int main() {
+
+    int arr[] = {1, 5, 7, -1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int K = 6;
+
+    int countPairsResult = countPairs(arr, size, K);
+    cout << "Number of pairs: " << countPairsResult << std::endl;
+
+    return 0;
+}
